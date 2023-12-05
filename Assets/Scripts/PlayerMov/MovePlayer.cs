4068e9bed385ba22a5519464c7e857d169e9a1b2
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-12-04 19:35:39 -0800

    fix player input "lag"

--- Assets/Scripts/PlayerMov/MovePlayer.cs
+++ Assets/Scripts/PlayerMov/MovePlayer.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     GridBehavior gridGenerator;
-    GridItemBehavior gridItemBehavior; 
+    GridItemBehavior gridItemBehavior;
     TurnBasedBehavior turnBasedBehavior;
-    public float movementTime = .3f; // time in seconds between each input read
+    public float movementTime = .2f; // time in seconds between each input read
 
     bool canMove = true;
 
-    enum Direction {
+    enum Direction
+    {
         UP,
         LEFT,
         DOWN,
@@ -25,16 +26,19 @@ public class NewBehaviourScript : MonoBehaviour
         turnBasedBehavior = GetComponent<TurnBasedBehavior>();
 
         GetStartPosition();
-        StartCoroutine(movementCountdown());
     }
 
     // temporary function that just grabs the first valid start position it finds and uses that
-    void GetStartPosition() {
+    void GetStartPosition()
+    {
         int startX = 0;
         int startY = 0;
-        for(startX = 0; startX < gridGenerator.rows; startX++) {
-            for (startY = 0; startY < gridGenerator.columns; startY++) {
-                if(gridGenerator.IsPositionValid(startX, startY)) {
+        for (startX = 0; startX < gridGenerator.rows; startX++)
+        {
+            for (startY = 0; startY < gridGenerator.columns; startY++)
+            {
+                if (gridGenerator.IsPositionValid(startX, startY))
+                {
                     gridItemBehavior.moveToPosition(startX, startY);
                     return;
                 }
@@ -47,60 +51,62 @@ public class NewBehaviourScript : MonoBehaviour
         float xDirection = Input.GetAxis("Horizontal");
         float yDirection = Input.GetAxis("Vertical");
 
-        if(canMove) {
-            if((xDirection != 0 || yDirection != 0) && turnBasedBehavior.TurnStarted()) {
-                Direction selectedDirection = GetCardinalDirection(xDirection, yDirection);
-                AttemptMovement(selectedDirection);
+        if (canMove && turnBasedBehavior.TurnStarted())
+        {
+            if (Input.GetButton("Down"))
+            {
+                AttemptMovement(Direction.DOWN);
             }
-            else if(Input.GetButton("Fire1") && turnBasedBehavior.TurnStarted()) {
+            else if (Input.GetButton("Up"))
+            {
+                AttemptMovement(Direction.UP);
+            }
+            else if (Input.GetButton("Left"))
+            {
+                AttemptMovement(Direction.LEFT);
+            }
+            else if (Input.GetButton("Right"))
+            {
+                AttemptMovement(Direction.RIGHT);
+            }
+            else if (Input.GetButton("Fire1"))
+            {
                 // temporary; skip your turn
                 Debug.Log("turn skipped");
+
                 turnBasedBehavior.EndTurn();
+                StartCoroutine(movementCountdown());
             }
-            canMove = false;
         }
     }
 
-    Direction GetCardinalDirection(float xInput, float yInput) {
-        Vector2 moveDirection = new Vector2(xInput, yInput).normalized;
-        float shortestDistance = 100;
-        int outIndex = 0;
-        Vector2[] directionArray = {Vector2.up, Vector2.left, Vector2.down, Vector2.right};
-
-        for(int i = 0; i < 4; i++) {
-            float thisDist = Vector2.Distance(moveDirection, directionArray[i]);
-            if(thisDist < shortestDistance) {
-                shortestDistance = thisDist;
-                outIndex = i;
-            }
-        }
-
-        return (Direction) outIndex;
-    }
-
-    void AttemptMovement(Direction direction) {
+    void AttemptMovement(Direction direction)
+    {
         Vector2Int targetPosition = gridItemBehavior.gridPosition;
         // select target grid position depending on inputted direction
-        switch(direction) {
-            case Direction.LEFT: {targetPosition.x -= 1; break;}
-            case Direction.RIGHT: {targetPosition.x += 1; break;}
-            case Direction.UP: {targetPosition.y += 1; break;}
-            case Direction.DOWN: {targetPosition.y -= 1; break;}
+        switch (direction)
+        {
+            case Direction.LEFT: { targetPosition.x -= 1; break; }
+            case Direction.RIGHT: { targetPosition.x += 1; break; }
+            case Direction.UP: { targetPosition.y += 1; break; }
+            case Direction.DOWN: { targetPosition.y -= 1; break; }
         }
-        
-        if(gridGenerator.IsPositionValid(targetPosition.x, targetPosition.y)) {
+
+        if (gridGenerator.IsPositionValid(targetPosition.x, targetPosition.y))
+        {
             gridItemBehavior.moveToPosition(targetPosition.x, targetPosition.y);
             turnBasedBehavior.EndTurn();
+            StartCoroutine(movementCountdown());
         }
     }
 
-// this is a really mcgwyver solution to get user input from axes
-// i think ideally this would instead work via button down
-// but god fucking damn
-    IEnumerator movementCountdown() {
-        while(true) {
-            yield return new WaitForSeconds(movementTime);
-            canMove = true;
-        }
+    // this is a really mcgwyver solution to get user input from axes
+    // i think ideally this would instead work via button down
+    // but god fucking damn
+    IEnumerator movementCountdown()
+    {
+        canMove = false;
+        yield return new WaitForSeconds(movementTime);
+        canMove = true;
     }
 }
