ef9c248144ff3e2959fdd5efdb2beeabd7667905
mohraielm <mohraielmatta@gmail.com>
2023-12-14 13:11:13 -0800

    surprise

--- Assets/TitleScreen.cs
+++ Assets/TitleScreen.cs
@@ -8,21 +8,33 @@ public class TitleScreen : MonoBehaviour
     public GameObject mainMenu;
     public GameObject aboutMenu;
 
+    public GameObject credits;
+
     public void Start()
     {
         mainMenu.SetActive(true);
         aboutMenu.SetActive(false);
+        credits.SetActive(false);
     }
 
     public void AboutMenu()
     {
         mainMenu.SetActive(false);
         aboutMenu.SetActive(true);
+        credits.SetActive(false);
     }
 
     public void MainMenu()
     {
         mainMenu.SetActive(true);
+        aboutMenu.SetActive(false);
+        credits.SetActive(false);
+
+    }
+    public void LoadCredits()
+    {
+        credits.SetActive(true);
+        mainMenu.SetActive(false);
         aboutMenu.SetActive(false);
     }
 }
