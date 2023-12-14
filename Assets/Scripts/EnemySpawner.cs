26b63395c7cd279bb57dc50f8c152c806d5467cf
MaryamDu <98149297+MaryamDu@users.noreply.github.com>
2023-12-14 02:57:40 -0800

    Making more enemies

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    //public TrapdoorSpawn trapdoorSpawn;
+
     public enum EnemyType {
         RANGLER,
         MINNOW,
@@ -24,9 +26,27 @@ public class EnemySpawner : MonoBehaviour
     {
         gridGenerator = GameObject.Find("GridGenerator").GetComponent<GridBehavior>();
 
-        // SpawnEnemyRandomly(EnemyType.MINNOW);
-        SpawnEnemyRandomly(EnemyType.SHARK);
-        SpawnEnemyRandomly(EnemyType.RANGLER);
+        int enemyNum = 1;
+        /*
+        if (trapdoorSpawn.goLevel1)
+        {
+            enemyNum = 7;
+        }
+        if (trapdoorSpawn.goLevel2)
+        {
+            enemyNum = 15;
+        }
+        if (trapdoorSpawn.goLevel3)
+        {
+            enemyNum = 20;
+        }*/
+        
+        // the actual number of enemies will be enemyNum * 2 (* 3 with minnow)
+        for (int i = enemyNum; i > 0; i--) {
+            SpawnEnemyRandomly(EnemyType.SHARK);
+            SpawnEnemyRandomly(EnemyType.RANGLER);
+            // SpawnEnemyRandomly(EnemyType.MINNOW);
+        }
     }
 
     void SpawnEnemyRandomly(EnemyType type) {
